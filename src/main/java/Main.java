bf4c1767f4ce22d44897a66799f69078a86db3bc
Natalie-Roach <natalie-roach@yandex.ru>
2022-06-28 13:47:33 +0300

    fixes #1 Main.java edited

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long expected = 30; //ожидаемый результат
+    long actual = service.calculate(1000_60, true); //фактический результат
+
+    System.out.println(expected + " == ? == " + actual);
   }
 }
 
